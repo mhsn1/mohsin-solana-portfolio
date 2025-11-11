16a8adf179181352206d1a656e8b693cf7b83c13
mhsn <mc@mhsn-MacBook-Pro.local>
2025-11-11 21:07:43 +0500

    Applied final Tailwind component fix in index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-// /pages/_app.js (New Code)
+// /pages/_app.js (Final Code)
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  // Hum yahan ek 'key' daal rahe hain taake Next.js/Turbopack force ho jaye
-  return <Component {...pageProps} key="app-root-load" />; 
+  // Yeh 'key' Tailwind compilation ko force kar sakta hai.
+  return <Component {...pageProps} key="app-root-fix" />; 
 }
 
 export default MyApp;
