83d2af832a8980c811d8409974e9a89d48e05e93
mhsn <mc@mhsn-MacBook-Pro.local>
2025-11-11 20:52:01 +0500

    Added final next.config.js for successful build

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Yeh line Next.js ko sahi tareeqay se chalane ke liye zaroori hai
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
