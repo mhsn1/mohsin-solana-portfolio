14332556e59c2f2538563611716429941bea6ef4
mhsn <mc@mhsn-MacBook-Pro.local>
2025-11-12 14:25:56 +0500

    FIX: Corrected image path to load directly from public folder

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <nav className="space-x-6 hidden md:flex">
           <a href="#expertise" className="text-gray-300 hover:text-blue-400 transition duration-300">Expertise</a>
           <a href="#history" className="text-gray-300 hover:text-blue-400 transition duration-300">History</a>
-          <a href="YOUR_GITHUB_LINK" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-blue-400 transition duration-300">GitHub</a>
+          <a href="https://github.com/mhsn1/Profile" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-blue-400 transition duration-300">GitHub</a>
           <a href="https://www.upwork.com/freelancers/~0164fcaefd2e237646?s=1517518458463281152" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-blue-400 transition duration-300">Upwork Profile</a>
         </nav>
 
